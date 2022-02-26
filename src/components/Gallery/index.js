018c56b1d2eeee2c1c1f55662426405d5fdd7be9
Christopher Speltz <chrisspeltz@msn.com>
2022-02-25 21:29:56 -0600

    complete feature/gallery branch

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -12,4 +12,15 @@ function Gallery(props) {
     </section>
   );
 }
+
+// function Gallery(props) {
+//   const currentCategory = props;
+//   return (
+//     <section>
+//       <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+//       <p>{currentCategory.description}</p>
+//       <PhotoList category={currentCategory.name} />
+//     </section>
+//   );
+// }
 export default Gallery;
