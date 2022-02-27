a5d832325d3f919efb0f9621b7c6ea002e61d44e
Christopher Speltz <chrisspeltz@msn.com>
2022-02-26 22:06:55 -0600

    20.5.3 compose the modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
+
 
 const PhotoList = ({ category }) => {
   const [photos] = useState([
@@ -120,8 +122,14 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter(photo => photo.category === category);
   console.log(currentPhotos)
+  
+  const toggleModal = (image, i) => {
+    // current photo
+  }
+
   return (
     <div>
+      <Modal />
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
@@ -129,6 +137,7 @@ const PhotoList = ({ category }) => {
             // src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
